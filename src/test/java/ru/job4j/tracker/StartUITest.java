0d2a8e20b9af4b3580fa9345e7baec5d335a584a
illia <235illino@gmail.com>
2020-06-07 11:41:01 +0300

    1.1. Тесты на ValidateInput.[223313#290349]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -121,6 +121,26 @@ public class StartUITest {
         assertThat(action.isCall(), is(true));
     }
 
-
+    @Test
+    public void whenInvalidExit() {
+        Output out = new StubOutput();
+        Input in = new StubInput(
+                new String[] { "63", "0" }
+        );
+        Tracker tracker = new Tracker();
+        UserAction[] actions = {
+                new ExitAction()
+        };
+        new StartUI(out).init(in, tracker, actions);
+        assertThat(out.toString(), is(
+                String.format(
+                        "Menu.%n"
+                                + "0. Exit%n"
+                                + "Wrong input, you can select: 0 .. 0%n"
+                                + "Menu.%n"
+                                + "0. Exit%n"
+                )
+        ));
+    }
 
 }
