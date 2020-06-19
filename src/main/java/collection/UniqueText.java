bb88b77ce7b500678d06bb604fbaea9953a684db
illia <235illino@gmail.com>
2020-06-19 11:25:17 +0300

    2. Идентичные тексты. [#290318]

---
+++ src/main/java/collection/UniqueText.java
@@ -0,0 +1,21 @@
+package collection;
+
+import java.util.HashSet;
+
+public class UniqueText {
+    public static boolean isEquals(String originText, String duplicateText) {
+        boolean rsl = true;
+        String[] origin = originText.split(" ");
+        String[] text = duplicateText.split(" ");
+        HashSet<String> check = new HashSet<>();
+        for (String orig : origin) {
+            check.add(orig);
+        }
+        for (String test : text) {
+            if (!check.contains(test)){
+                rsl = false;
+            }
+        }
+        return rsl;
+    }
+}
