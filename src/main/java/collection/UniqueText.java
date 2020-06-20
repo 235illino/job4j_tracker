91dbdb32bfe192646caac4b5ec031acab6db0934
illia <235illino@gmail.com>
2020-06-20 13:09:43 +0300

    2. Идентичные тексты. [#290318]

--- src/main/java/collection/UniqueText.java
+++ src/main/java/collection/UniqueText.java
@@ -14,6 +14,7 @@ public class UniqueText {
         for (String test : text) {
             if (!check.contains(test)){
                 rsl = false;
+                break;
             }
         }
         return rsl;
