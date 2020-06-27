adb6595f80cc97d7a2cefc0ce9e167e26d93eba1
illia <235illino@gmail.com>
2020-06-27 19:38:18 +0300

    2.0. Встроенные функциональные интерфейсы. [#290162]

--- src/main/java/lambda/SearchAtt.java
+++ src/main/java/lambda/SearchAtt.java
@@ -2,7 +2,9 @@ package lambda;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Function;
 import java.util.function.Predicate;
+import java.util.function.UnaryOperator;
 
 public class SearchAtt {
 
@@ -16,13 +18,6 @@ public class SearchAtt {
         return rsl;
     }
 
-    Predicate<Attachment> filter100 = new Predicate<Attachment>() {
-        @Override
-        public boolean test(Attachment attachment) {
-           return attachment.getSize() > 100;
-        }
-    };
-
     public static List<Attachment> filterName(List<Attachment> list) {
         List<Attachment> rsl = new ArrayList<>();
         for (Attachment att : list) {
@@ -32,4 +27,27 @@ public class SearchAtt {
         }
         return rsl;
     }
+    UnaryOperator<List<Attachment>> unary = new UnaryOperator<List<Attachment>>() {
+        @Override
+        public List<Attachment> apply(List<Attachment> attachments) {
+            Predicate<Attachment> filter100 = new Predicate<Attachment>() {
+                @Override
+                public boolean test(Attachment attachment) {
+                    return attachment.getSize() > 100;
+                }
+            };
+            Predicate<Attachment> filterName = new Predicate<Attachment>() {
+                @Override
+                public boolean test(Attachment attachment) {
+                    return attachment.getName().contains("bug");
+                }
+            };
+            for (Attachment att : attachments) {
+                if (filter100.test(att) || filterName.test(att)) {
+                    attachments.add(att);
+                }
+            }
+            return attachments;
+        }
+    };
 }
