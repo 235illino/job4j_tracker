9c968d4eed845a6f74510e82e1202e3e69b74e99
illia <235illino@gmail.com>
2020-06-28 11:22:53 +0300

    2.0. Встроенные функциональные интерфейсы. [#290162]

--- src/main/java/lambda/SearchAtt.java
+++ src/main/java/lambda/SearchAtt.java
@@ -1,53 +1,26 @@
 package lambda;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.function.UnaryOperator;
 
 public class SearchAtt {
 
-    public static List<Attachment> filterSize(List<Attachment> list) {
-        List<Attachment> rsl = new ArrayList<>();
-        for (Attachment att : list) {
-            if (att.getSize() > 100) {
-                rsl.add(att);
-            }
-        }
-        return rsl;
-    }
+    public static List<Attachment> filterSize(List<Attachment> list, Predicate<Attachment> filter) {
 
-    public static List<Attachment> filterName(List<Attachment> list) {
-        List<Attachment> rsl = new ArrayList<>();
-        for (Attachment att : list) {
-            if (att.getName().contains("bug")) {
-                rsl.add(att);
+        UnaryOperator<List<Attachment>> unary = new UnaryOperator<List<Attachment>>() {
+            @Override
+            public List<Attachment> apply(List<Attachment> attachments) {
+
+                for (Attachment att : attachments) {
+                    if (filter.test(att)) {
+                        attachments.add(att);
+                    }
+                }
+                return attachments;
             }
-        }
-        return rsl;
+        };
+
+        return unary.apply(list);
     }
-    UnaryOperator<List<Attachment>> unary = new UnaryOperator<List<Attachment>>() {
-        @Override
-        public List<Attachment> apply(List<Attachment> attachments) {
-            Predicate<Attachment> filter100 = new Predicate<Attachment>() {
-                @Override
-                public boolean test(Attachment attachment) {
-                    return attachment.getSize() > 100;
-                }
-            };
-            Predicate<Attachment> filterName = new Predicate<Attachment>() {
-                @Override
-                public boolean test(Attachment attachment) {
-                    return attachment.getName().contains("bug");
-                }
-            };
-            for (Attachment att : attachments) {
-                if (filter100.test(att) || filterName.test(att)) {
-                    attachments.add(att);
-                }
-            }
-            return attachments;
-        }
-    };
 }
