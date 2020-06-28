374e00b458ffb1fce417d6092adbcf522bffb64d
illia <235illino@gmail.com>
2020-06-28 22:13:24 +0300

    2.0. Встроенные функциональные интерфейсы. [#290162]

--- src/main/java/search/PhoneDictionary.java
+++ src/main/java/search/PhoneDictionary.java
@@ -16,25 +16,21 @@ public class PhoneDictionary {
      * @param key Ключ поиска.
      * @return Список подощедщих пользователей.
      */
+
     public ArrayList<Person> find(String key) {
+        Predicate<Person> combine = new Predicate<Person>() {
+            @Override
+            public boolean test(Person person) {
+                return (person.getAddress().contains(key) || person.getName().contains(key) || person.getPhone().contains(key) || person.getSurname().contains(key));
+            }
+        };
         ArrayList<Person> result = new ArrayList<>();
         for (Person person : persons) {
-            if (person.getAddress().contains(key) || person.getName().contains(key) || person.getPhone().contains(key) || person.getSurname().contains(key)) {
+            if (combine.test(person)) {
                 result.add(person);
             }
         }
         return result;
     }
-
-//    public ArrayList<Person> find(String key) {
-//        Predicate<Person> combine = new Predicate<Person>()
-//        ArrayList<Person> result = new ArrayList<>();
-//        for (Person person : persons) {
-//            if (combine.test(person)) {
-//                result.add(person);
-//            }
-//        }
-//        return result;
-//    }
 
 }
