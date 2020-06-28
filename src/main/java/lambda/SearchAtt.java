12ab1d64d29aab8298950e16df2b19883980ded3
illia <235illino@gmail.com>
2020-06-28 22:30:16 +0300

    2.0. Встроенные функциональные интерфейсы. [#290162]

--- src/main/java/lambda/SearchAtt.java
+++ src/main/java/lambda/SearchAtt.java
@@ -1,26 +1,34 @@
 package lambda;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Predicate;
 import java.util.function.UnaryOperator;
 
 public class SearchAtt {
 
-    public static List<Attachment> filterSize(List<Attachment> list, Predicate<Attachment> filter) {
-
-        UnaryOperator<List<Attachment>> unary = new UnaryOperator<List<Attachment>>() {
-            @Override
-            public List<Attachment> apply(List<Attachment> attachments) {
-
-                for (Attachment att : attachments) {
-                    if (filter.test(att)) {
-                        attachments.add(att);
-                    }
-                }
-                return attachments;
+    public static List<Attachment> filterSize(List<Attachment> list, Predicate<Attachment> predicate) {
+        List<Attachment> rsl = new ArrayList<>();
+        for (Attachment att : list) {
+            if (predicate.test(att)) {
+                rsl.add(att);
             }
-        };
+        }
+        return rsl;
+    }
 
-        return unary.apply(list);
+    public static List<Attachment> filterName(List<Attachment> list, Predicate<Attachment> predicate) {
+        List<Attachment> rsl = new ArrayList<>();
+        for (Attachment att : list) {
+            if (predicate.test(att)) {
+                rsl.add(att);
+            }
+        }
+        return rsl;
+    }
+
+    public static Predicate<Attachment> filter(Attachment att) {
+        Predicate<Attachment> predicate = attachment -> (att.getName().contains("bug") || att.getSize() > 100);
+        return predicate;
     }
 }
