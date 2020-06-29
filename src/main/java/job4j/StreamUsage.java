9a49228061f88b6375ebc9c897961ce954792d50
illia <235illino@gmail.com>
2020-06-29 22:43:51 +0300

    0. Stream API [#290167]

---
+++ src/main/java/job4j/StreamUsage.java
@@ -0,0 +1,21 @@
+package job4j;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class StreamUsage {
+    public static void main(String[] args) {
+        List<Integer> nums = Arrays.asList(
+                -1,
+                5,
+                -3,
+                8,
+                -8,
+                10,
+                22
+        );
+        List<Integer> numsPlus = nums.stream().filter(num -> num > 0).collect(Collectors.toList());
+        numsPlus.forEach(System.out::println);
+    }
+}
