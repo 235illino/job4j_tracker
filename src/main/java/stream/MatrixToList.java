944eded34aa501ec3daae7d6d033fbd3cd9e488b
illia <235illino@gmail.com>
2020-07-01 22:51:16 +0300

    5. Преобразование матрицы чисел в список чисел. [#290172]

---
+++ src/main/java/stream/MatrixToList.java
@@ -0,0 +1,11 @@
+package stream;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class MatrixToList {
+    public static List<Integer> matrixTo (Integer[][] matrix) {
+        return Arrays.stream(matrix).flatMap(Arrays::stream).collect(Collectors.toList());
+    }
+}
---
+++ src/test/java/stream/MatrixToListTest.java
@@ -0,0 +1,22 @@
+package stream;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
+public class MatrixToListTest {
+    @Test
+    public void name() {
+        Integer[][] integers = {{5,7,3,17}, {7,0,1,12}, {8,1,2,3}};
+        List<Integer> exp = Arrays.asList(
+                5, 7, 3, 17, 7, 0, 1, 12, 8, 1, 2, 3
+        );
+        List<Integer> rsl = MatrixToList.matrixTo(integers);
+        assertThat(exp, is(rsl));
+
+    }
+}
