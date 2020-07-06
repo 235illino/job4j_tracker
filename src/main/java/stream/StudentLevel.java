b3c88162e2bde3601d847a5ae0f6556da8fb11f6
illia <235illino@gmail.com>
2020-07-06 11:52:51 +0300

    0. Stream API улучшения [#290177]

---
+++ src/main/java/stream/StudentLevel.java
@@ -0,0 +1,16 @@
+package stream;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+public class StudentLevel {
+    public static List<Student> levelOf(List<Student> students, int bound) {
+        return students.stream()
+                .filter(st -> !Objects.isNull(st))
+                .sorted(Comparator.comparingInt(Student::getScore))
+                .takeWhile(st -> st.getScore() > bound)
+                .collect(Collectors.toList());
+    }
+}
